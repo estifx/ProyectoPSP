1b6fe6c668cf3930309b2236994b09cf06ffa757
alumno <alumno@INFORMATICA-22>
2023-02-07 20:03:50 +0100

    Base de datos

--- PSP/src/HiloServidor.java
+++ PSP/src/HiloServidor.java
@@ -1,29 +1,24 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import entrada.Teclado;
 
-public class HiloServidor {
-	public static void main(String args[]) throws IOException { 
-		int codigo;
-		ServerSocket servidor; 
-		servidor = new ServerSocket(60000);    
-
-		System.out.println("--------  Servidor -----------");
-		System.out.println("Servidor iniciado...\n");
-		System.out.println("1 para iniciar sesión");
-		System.out.println("2 realizar transferencia");
-		System.out.println("0 para salir");
-
-		codigo = Teclado.leerEntero("Opción a elegir:");
-		while (true) { 
-			Socket cliente = new Socket(); 
-
-			// Espera Cliente
-			cliente = servidor.accept();//esperando cliente
-
-		}
-
+public class HiloServidor extends Thread {
+	
+	BufferedReader fentrada; 
+	PrintWriter fsalida; 
+	Socket cliente = null; 
+	
+	public HiloServidor(Socket cliente) throws IOException {
+		this.cliente = cliente; 
+		fsalida = new PrintWriter(cliente.getOutputStream(), true); 
+		fentrada = new BufferedReader(new InputStreamReader(cliente.getInputStream())); 
 	}
+	
+	
+	
 }
--- PSP/src/Main.java
+++ PSP/src/Main.java
@@ -1,11 +1,30 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import entrada.Teclado;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("hnnnnnnaaaanni");
-		System.out.println("hello there");
+	public static void main(String args[]) throws IOException { 
+		int codigo;
+		ServerSocket servidor; 
+		servidor = new ServerSocket(60000);    
 
-			System.out.println();
+		System.out.println("--------  Servidor -----------");
+		System.out.println("Servidor iniciado...\n");
+		System.out.println("1 para iniciar sesión");
+		System.out.println("2 realizar transferencia");
+		System.out.println("0 para salir");
+
+		codigo = Teclado.leerEntero("Opción a elegir:");
+		while (true) { 
+			Socket cliente = new Socket(); 
+
+			// Espera Cliente
+			cliente = servidor.accept();//esperando cliente
+
+		}
+
 	}
-
 }
