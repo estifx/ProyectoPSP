80d0704dbcd56195264b14650e8b785f6240b22a
lunav <lunav@DESKTOP-NE1F7G5>
2023-02-25 11:14:59 +0100

    Servidor SSL

--- PSP/src/programa/Servidor.java
+++ PSP/src/programa/Servidor.java
@@ -4,21 +4,34 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+
 public class Servidor {
 
-	public static void main(String[] args) {
-		ServerSocket servidor;
+public static void main(String[] args) {
+		
+		System.setProperty("javax.net.ssl.keyStore", "./Certificados SSL/AlmacenServidorSSL"); 
+		System.setProperty("javax.net.ssl.keyStorePassword", "951357"); 
+		
+		int puerto= 60000;
+
 		try {
-			servidor = new ServerSocket(60000);
+			
+			SSLServerSocketFactory serverSSL = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault(); 
+			SSLServerSocket servidor = (SSLServerSocket) serverSSL.createServerSocket(puerto);
+			SSLSocket cliente = null;
+			
 			while (true) { 
 				System.out.println("--------  Servidor -----------");
 				System.out.println("Servidor iniciado...\n");
 				System.out.println("Esperando Cliente...\n");
 
-				Socket cliente = new Socket(); 
+				
 
 				// Espera Cliente
-				cliente = servidor.accept();//esperando cliente
+				cliente = (SSLSocket)servidor.accept();//esperando cliente
 
 				System.out.println("Cliente conectado\n");
 				// Crea hilo y lo lanza servidor donde se atiende al cliente
